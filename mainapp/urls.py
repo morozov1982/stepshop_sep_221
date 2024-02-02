88911e7e8c8d3d215d5e1b47d3a4add710665b0e
Морозов Александр <@step.kz>
2024-02-02 20:21:35 +0600

    lesson_05 start

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, products
+from .views import index, products, product, about, contacts
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('product/', product, name='product'),
+    path('about/', about, name='about'),
+    path('contacts/', contacts, name='contacts'),
 ]
